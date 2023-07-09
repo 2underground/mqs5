16d0a627c7261c0efec4d185e0e6ba63213263c1
2underground <m.andriyuk@gmail.com>
2023-07-10 00:22:28 +0300

    0
    s5

---
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIColor {
+    static let ypBlack = UIColor(named: "black")
+    static let ypRed = UIColor(named: "red")
+    static let ypGreen = UIColor(named: "green")
+    static let ypGray = UIColor(named: "gray")
+    static let ypBackground = UIColor(named: "background")
+}
